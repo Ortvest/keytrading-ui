324dacc657998e48a34f21f531fd70bf86e3477d
keydies <serkeyides@gmail.com>
2025-01-09 14:54:03 +0200

    [KTAI-2] feat: added chakra ui v2

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from 'react-dom/client';
 
+import { ChakraProvider } from '@chakra-ui/react';
 import { Main } from '@pages/Main/Main';
 
-createRoot(document.getElementById('root')!).render(<Main />);
+createRoot(document.getElementById('root')!).render(
+  <ChakraProvider>
+    <Main />
+  </ChakraProvider>
+);
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,5 +1,24 @@
+import { Button, Stack } from '@chakra-ui/react';
+
 import '@shared/styles/global.css';
 
 export const Main = () => {
-  return <div>Main</div>;
+  return (
+    <div>
+      <Stack spacing={4} direction="row" align="center">
+        <Button colorScheme="teal" size="xs">
+          Button
+        </Button>
+        <Button colorScheme="teal" size="sm">
+          Button
+        </Button>
+        <Button colorScheme="teal" size="md">
+          Button
+        </Button>
+        <Button colorScheme="teal" size="lg">
+          Button
+        </Button>
+      </Stack>
+    </div>
+  );
 };
