5e85176210ebd03a797481df5fc59fec3043a6c0
Serhii Schastlivtsev <serkeyides@gmail.com>
2025-01-10 17:37:18 +0200

    fix: delete unused imports

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,17 +1,17 @@
-import { LoginModal } from '@modules/Modals/Login';
 import { Header } from '@modules/Header';
-        
-import { Box, Button, Stack } from '@chakra-ui/react';
+import { LoginModal } from '@modules/Modals/Login';
+
+import { Box } from '@chakra-ui/react';
 
 import '@shared/styles/global.css';
 
 export const Main = () => {
   return (
-     <>
+    <>
       <Header />
       <Box background="#444444" minHeight="100vh">
         <LoginModal />
       </Box>
-     </>
+    </>
   );
 };
