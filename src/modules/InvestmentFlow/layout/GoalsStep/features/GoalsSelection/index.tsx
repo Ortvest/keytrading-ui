725a94e56892a30285f71c4187cde0b99579e220
Maksymus333333 <mdachevskyi@edu.cdv.pl>
2025-01-16 16:22:14 +0100

    [KTAI-18] ref: edited the code

--- src/modules/InvestmentFlow/layout/GoalsStep/features/GoalsSelection/index.tsx
+++ src/modules/InvestmentFlow/layout/GoalsStep/features/GoalsSelection/index.tsx
@@ -4,15 +4,15 @@ import { goalsMockData } from '@shared/mocks/Goals.mocks';
 import { Checkbox, CheckboxGroup, Grid } from '@chakra-ui/react';
 
 interface GoalsSelectionProps {
-  onGoalsChange: (selectedGoals: string[]) => void;
+  onGoalsChangeHanlder: (selectedGoals: string[]) => void;
 }
 
-export const GoalsSelection = ({ onGoalsChange }: GoalsSelectionProps) => {
+export const GoalsSelection = ({ onGoalsChangeHanlder }: GoalsSelectionProps) => {
   return (
-    <CheckboxGroup onChange={onGoalsChange}>
+    <CheckboxGroup onChange={onGoalsChangeHanlder}>
       <Grid templateColumns="repeat(2, 1fr)" gap={4}>
-        {goalsMockData.map((goal: Goal) => (
-          <Checkbox key={goal.value} value={goal.value}>
+        {goalsMockData.map((goal: Goal, index: number) => (
+          <Checkbox key={index} value={goal.value}>
             {goal.name}
           </Checkbox>
         ))}
--- src/modules/InvestmentFlow/layout/GoalsStep/index.tsx
+++ src/modules/InvestmentFlow/layout/GoalsStep/index.tsx
@@ -5,17 +5,17 @@ import { GoalsSelection } from '@modules/InvestmentFlow/layout/GoalsStep/feature
 import { Box, Flex } from '@chakra-ui/react';
 
 interface GoalsStepProps {
-  onGoalsChange: (selectedGoals: string[]) => void;
+  onGoalsChangeHanlder: (selectedGoals: string[]) => void;
 }
 
-export const GoalsStep = ({ onGoalsChange }: GoalsStepProps) => {
+export const GoalsStep = ({ onGoalsChangeHanlder }: GoalsStepProps) => {
   return (
     <Box>
       <Flex direction="column" align="center" justify="center" p="20px" minH="100vh" bg="#f8f9fa">
         <Box bg="white" p="20px" borderRadius="md" boxShadow="md" maxW="400px" w="100%">
           <GoalsInvestment />
           <Flex direction="column" gap="12px">
-            <GoalsSelection onGoalsChange={onGoalsChange} />
+            <GoalsSelection onGoalsChangeHanlder={onGoalsChangeHanlder} />
             <NavigationButton />
           </Flex>
         </Box>
