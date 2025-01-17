b02f28d0de0c817663adcdcca54bae3ae76d0868
Maksymus333333 <mdachevskyi@edu.cdv.pl>
2025-01-17 13:17:13 +0100

    [KTAI-19] feat: added third step of this flow

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -2,6 +2,7 @@ import { Header } from '@modules/Header';
 import { AmountStep } from '@modules/InvestmentFlow/layout/AmountStep';
 import { CompaniesStep } from '@modules/InvestmentFlow/layout/CompaniesStep';
 import { GoalsStep } from '@modules/InvestmentFlow/layout/GoalsStep';
+import { InterestedStep } from '@modules/InvestmentFlow/layout/InterestedStep';
 import { LoginModal } from '@modules/Modals/Login';
 
 import { Box } from '@chakra-ui/react';
@@ -16,8 +17,9 @@ export const Main = () => {
         <LoginModal />
       </Box>
       <AmountStep />
-      <CompaniesStep />
       <GoalsStep />
+      <InterestedStep />
+      <CompaniesStep />
     </>
   );
 };
---
+++ src/shared/mocks/Sectors.mocks.ts
@@ -0,0 +1,15 @@
+import { Sector } from '@shared/interfaces/Sector.interface';
+
+export const sectorsMockData: Sector[] = [
+  { name: 'Healthcare', value: 'healthcare' },
+  { name: 'Financials', value: 'financials' },
+  { name: 'Technology', value: 'technology' },
+  { name: 'Industrials', value: 'industrials' },
+  { name: 'Consumer Discretionary', value: 'consumer-discretionary' },
+  { name: 'Materials', value: 'materials' },
+  { name: 'Real Estate', value: 'real-estate' },
+  { name: 'Communication Services', value: 'communication-services' },
+  { name: 'Energy', value: 'energy' },
+  { name: 'Consumer Staples', value: 'consumer-staples' },
+  { name: 'Utilities', value: 'utilities' },
+];
